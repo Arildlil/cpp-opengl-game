229f1cec038678117661aaf6ec102ba5a3de5b70
arildlil <arildlil@student.matnat.uio.no>
2018-08-25 14:31:00 +0200

    fix Fixed the flickering issue

--- src/app.cpp
+++ src/app.cpp
@@ -10,11 +10,34 @@ void cleanup() {
     glfwTerminate();
 }
 
+void process_input(Core::Window& window) {
+    if (glfwGetKey(window.get_window(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window.get_window(), true);
+    }
+}
+
+void game_loop(Core::Window& game_window) {
+    GLFWwindow* main_window {game_window.get_window()};
+    while (!glfwWindowShouldClose(main_window)) {
+        // input handling
+        process_input(game_window);
+
+        // rendering
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check/call events and swap buffers
+        glfwSwapBuffers(main_window);
+        glfwPollEvents();
+    }
+}
+
 int main(int argc, char **argv) {
     
     glfwInit();
     
     Core::Window game_window {800, 600, "My First OpenGL Window"};
+
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Error: Failed to initialize GLAD!" << std::endl;
@@ -23,12 +46,8 @@ int main(int argc, char **argv) {
     }
 
     game_window.show();
-    
-    GLFWwindow* main_window {game_window.get_window()};
-    while (!glfwWindowShouldClose(main_window)) {
-        glfwSwapBuffers(main_window);
-        glfwPollEvents();
-    }
+
+    game_loop(game_window);
 
     std::cout << "Closing app..." << std::endl;
 
