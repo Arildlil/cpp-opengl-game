6eff5e3bfee844aee4bf5e6fcee47c26225ad761
arildlil <arildlil@student.matnat.uio.no>
2018-09-03 16:05:58 +0200

    feat Finished all the required setup code to render a box on the screen. TODO: Complete refactoring and tidying of the code!

--- src/app.cpp
+++ src/app.cpp
@@ -8,14 +8,14 @@
 #include <fstream>
 #include <string>
 
-//static std::vector< std::vector<char> > shaders {};
 
-std::vector<std::string>& load_shaders(const std::vector<const char*>& files, 
+
+std::vector<std::string>& load_shader_files(const std::vector<const char*>& files, 
     std::vector<std::string>& output) 
 {
     for (const char* file_name : files) {
         std::ifstream ifs {file_name};
-        std::string temp (512, '\0');
+        std::string temp {};
         if (!ifs) {
             std::cerr << "Invalid file name: " << file_name << std::endl;
             continue;
@@ -26,10 +26,30 @@ std::vector<std::string>& load_shaders(const std::vector<const char*>& files,
         while (ifs.get(c)) {
             temp += c;
         }
+        temp += '\0';
         output.push_back(temp);
     }
 
     return output;
+}
+
+bool compile_shader(GLuint shader_id, const std::string& shader_data) {
+    int success {0};
+    constexpr int max_info_len {512};
+    char info_log[max_info_len];
+
+    const GLchar* cdata {shader_data.c_str()};
+    glShaderSource(shader_id, 1, &cdata, NULL);
+    glCompileShader(shader_id);
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader_id, max_info_len, NULL, info_log);
+        std::cerr << "Shader " << shader_id << ": " << info_log << std::endl;
+        glDeleteShader(shader_id);
+        return false;
+    }
+
+    return true;
 }
 
 void cleanup() {
@@ -42,22 +62,6 @@ void process_input(Core::Window& window) {
     }
 }
 
-void game_loop(Core::Window& game_window) {
-    GLFWwindow* main_window {game_window.get_window()};
-    while (!glfwWindowShouldClose(main_window)) {
-        // input handling
-        process_input(game_window);
-
-        // rendering
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // check/call events and swap buffers
-        glfwSwapBuffers(main_window);
-        glfwPollEvents();
-    }
-}
-
 int main(int argc, char **argv) {
     
     glfwInit();
@@ -66,8 +70,8 @@ int main(int argc, char **argv) {
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Error: Failed to initialize GLAD!" << std::endl;
-        //cleanup();
-        //return -1;
+        cleanup();
+        return -1;
     }
 
 
@@ -99,18 +103,71 @@ int main(int argc, char **argv) {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
 
     // Setup shaders
     const std::vector<const char*> shader_files {"src/minimal.vert", "src/minimal.frag"};
     std::vector<std::string> shader_contents {};
-    load_shaders(shader_files, shader_contents);
-    for (std::string shader : shader_contents) {
-        std::cout << shader << std::endl;
+    load_shader_files(shader_files, shader_contents);
+
+    // Compiler shaders
+    std::vector<GLuint> shaders {glCreateShader(GL_VERTEX_SHADER), glCreateShader(GL_FRAGMENT_SHADER)};
+    for (int i {0}; i < shaders.size() && i < shader_files.size(); i++) {
+        if (!compile_shader(shaders[i], shader_contents[i])) {
+            cleanup();
+            return -1;   
+        }
+    }
+
+    // Create and link shader program
+    GLuint shader_program {glCreateProgram()};
+    for (GLuint shader_id : shaders) {
+        glAttachShader(shader_program, shader_id);
+    }
+    
+    int success {0};
+    constexpr int max_info_len {512};
+    char info_log[max_info_len];
+    
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, max_info_len, NULL, info_log);
+        std::cerr << "Shader linking: " << info_log << std::endl;
+        cleanup();
+        return -1;
+    }
+
+    for (GLuint shader_id : shaders) {
+        glDeleteShader(shader_id);
     }
 
     game_window.show();
-    game_loop(game_window);
+
+    GLFWwindow* main_window {game_window.get_window()};
+    while (!glfwWindowShouldClose(main_window)) {
+        // input handling
+        process_input(game_window);
+
+        // rendering
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        // check/call events and swap buffers
+        glfwSwapBuffers(main_window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     std::cout << "Closing app..." << std::endl;
 
